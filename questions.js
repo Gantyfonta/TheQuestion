d3089a5d84cf20f3f9ba14a58b35d30d838687fb
Gantyfonta <28gkarfonta@catholiccentral.net>
2026-03-02 08:40:24 -0500

    Add initial polls data structure

---
+++ questions.js
@@ -0,0 +1,14 @@
+const polls = {
+  "2026-03-02": {
+    question: "Is cereal soup?",
+    answers: ["Yes", "No"]
+  },
+  "2026-03-03": {
+    question: "Best season?",
+    answers: ["Spring", "Summer", "Fall", "Winter"]
+  },
+  "2026-03-04": {
+    question: "Pineapple on pizza?",
+    answers: ["Love it", "Hate it", "Never tried", "It's okay"]
+  }
+};
