5474e284b420dce02fd07f21781f5ea503d70bfe
Gantyfonta <28gkarfonta@catholiccentral.net>
2026-03-03 12:44:14 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,15 +7,19 @@ export const polls = {
     question: "Is water wet?",
     answers: ["Yes", "No", "Life is a simulation, therefore its fake."]
   },
-  "2026-03-04": {
+   "2026-03-04": {
+    question: "Who would win? A chipmunk or a gorilla? (They are the same size)",
+    answers: ["Chipmunk", "Gorilla"]
+  },
+  "2026-03-05": {
     question: "Pineapple on pizza?",
     answers: ["Yes. As it should be.", "No, but I'll change my mind."]
   },
-"2026-03-05": {
+"2026-03-6": {
     question: "Is a hotdog a sandwitch?",
     answers: ["Yes.", "No."]
   },
-  "2026-03-06": {
+  "2026-03-07": {
     question: "Is ketchup a smoothie?",
     answers: ["Yes.", "No."]
   },
